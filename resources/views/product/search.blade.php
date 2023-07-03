1f3589c477340422dcaf904f00c9b8a14e5ceb5f
Quoc Thoai <thoaifamily83@gmail.com>
2023-07-03 19:49:32 +0700

    fixed show result search

--- resources/views/product/search.blade.php
+++ resources/views/product/search.blade.php
@@ -1,6 +1,10 @@
 @extends('main')
 
 @section('content')
+<div class="bg0 p-b-20">
+    <div class="container">
     @include('product.list')
     {!! $products->links() !!}
+    </div>
+</div>
 @endsection
